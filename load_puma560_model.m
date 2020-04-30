de707abbcd7a8418b0cef6129dd9ab9441cd0365
fabriziodiblasi <fabriziodiblasi92@gmail.com>
2020-04-30 16:07:55 +0200

    calcolo cinematica diretta

---
+++ load_puma560_model.m
@@ -0,0 +1,10 @@
+function puma560_model = load_puma560_model(d, a, alpha)
+    
+    
+
+    for i=1:6
+        L(i) = Link('d', d(i), 'a', a(i), 'alpha', alpha(i), 'revolute');
+    end
+
+    puma560_model = SerialLink(L, 'name','puma560_model');
+end
