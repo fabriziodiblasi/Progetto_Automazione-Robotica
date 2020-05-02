008bef2aa8354b9bf12a2c81d89e7347b6fe0125
fabriziodiblasi <fabriziodiblasi92@gmail.com>
2020-05-02 18:36:38 +0200

    aggiunta bozza di relazione

--- main.m
+++ main.m
@@ -17,46 +17,42 @@ d = [0.0,  0.0,  0.1501,  0.4331,  0.0,  0.0];
 puma50_model = load_puma560_model(d, a, alpha);
 
 % X = 4
-%q(1) = [0 0 0 0 0 0]
-q_1 = [0,0,0,0,0,0];
-%q(2) = [0 0 ? ?/? 0 0 0]
-q_2 = [0,0,-pi/4,0,0,0];
-%q(3) = [0 ?/? ? ? 0 0 0]
-q_3 = [0,pi/4,-pi,0,0,0];
-%q(4) = [0 ?/4 ? ? 0 ?/? 0]
-q_4 = [0,pi/4,-pi,0,pi/4,0];
-%q(5) = [0 0 ? ?/2 ?/6 0 ? ?/?]
-q_5 = [0,0,-pi/2,pi/6,0,-pi/4];
-%q(6) = [0 ? ?/? 0 0 ? ?/4 0]
-q_6 = [0,-pi/4,0,0,-pi/4,0];
+% %q(1) = [0 0 0 0 0 0]
+% q_1 = [0,0,0,0,0,0];
+% %q(2) = [0 0 ? ?/? 0 0 0]
+% q_2 = [0,0,-pi/4,0,0,0];
+% %q(3) = [0 ?/? ? ? 0 0 0]
+% q_3 = [0,pi/4,-pi,0,0,0];
+% %q(4) = [0 ?/4 ? ? 0 ?/? 0]
+% q_4 = [0,pi/4,-pi,0,pi/4,0];
+% %q(5) = [0 0 ? ?/2 ?/6 0 ? ?/?]
+% q_5 = [0,0,-pi/2,pi/6,0,-pi/4];
+% %q(6) = [0 ? ?/? 0 0 ? ?/4 0]
+% q_6 = [0,-pi/4,0,0,-pi/4,0];
 
 
-matrice_vettori_q= [ 0,0,0,0,0,0;...
-                     0,0,-pi/4,0,0,0;...
-                     0,pi/4,-pi,0,0,0;...
-                     0,pi/4,-pi,0,pi/4,0;...
-                     0,0,-pi/2,pi/6,0,-pi/4;...
-                     0,-pi/4,0,0,-pi/4,0];
+Q= [ 0,0,0,0,0,0;...
+     0,0,-pi/4,0,0,0;...
+     0,pi/4,-pi,0,0,0;...
+     0,pi/4,-pi,0,pi/4,0;...
+     0,0,-pi/2,pi/6,0,-pi/4;...
+     0,-pi/4,0,0,-pi/4,0];
 
 
 %cinematica diretta tramite CORKE
-%fkine(puma50_model, q_1)
-fkine(puma50_model, q_2)
-%fkine(puma50_model, q_3)
-%fkine(puma50_model, q_4)
-%fkine(puma50_model, q_5)
-%fkine(puma50_model, q_6)
-
-for i=1:6
-    A = calcolo_matrice_trasformazione(alpha(i), q_2(i), d(i), a(i));
-    if i == 1
-        T = A;
-    elseif i>1
-        T = T * A;
+for row=1:6
+    for col=1:6
+        A = calcolo_matrice_trasformazione(alpha(col), Q(row,col), d(col), a(col));
+        if col == 1
+            T = A;
+        elseif col>1
+            T = T * A;
+        end
     end
+    T %matrice calcolata manualmente sostituendo i vettori di giunto al posto del parametro theta
+    fkine(puma50_model, Q(row,:)) %matrici della cinematica diretta calcolate con il toolbox di corke
 end
 
-T
 
 
 
